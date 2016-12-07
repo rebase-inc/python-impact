6dde4210c45512f5497c8160dbfb8a481dbfe98b
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2016-12-06 22:56:40 -0800

    Fixed common layout

--- clean.sh
+++ clean.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker-compose -f layouts/dev.yml -f layouts/pro.yml stop
 docker-compose -f layouts/dev.yml -f layouts/pro.yml rm
-docker rmi $(docker images -a --filter=dangling=true -q)
