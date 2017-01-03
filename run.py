c74fc857c86d6c04dd3acbb83c19a15221c45f4d
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-01-02 21:57:21 -0800

    Switch to using json blob instead of bytearray

--- run.py
+++ run.py
@@ -1,41 +1,37 @@
 import os
+import json
+import errno
 import base64
 import logging
+from multiprocessing import current_process
 
-import rsyslog 
+import rsyslog
 from curio_http import ClientSession
-from asynctcp import AsyncTCPCallbackServer
+from asynctcp import AsyncTcpCallbackServer
 
-    
+current_process().name = os.environ['HOSTNAME']
 rsyslog.setup(log_level = os.environ['LOG_LEVEL'])
 LOGGER = logging.getLogger(__name__)
 
-async def get_downloads(package_name):
+async def get_downloads(json_request):
     async with ClientSession() as session:
-        url = 'https://pypi.python.org/pypi/{}/json'.format(package_name)
+        url = 'https://pypi.python.org/pypi/{}/json'.format(json_request['module'])
         response = await session.get(url)
         if response.status_code == 404:
-            LOGGER.debug('No such package {}'.format(package_name))
-            return 0
+            LOGGER.debug('No such package {}'.format(json_request['module']))
+            data = { 'impact': 0 }
         elif (response.status_code < 200) or (response.status_code >= 300):
             LOGGER.error('GET of {} returned {}'.format(url, response.status_code))
-            return 0
-        content = await response.json()
-        if 'releases' not in content:
-            LOGGER.error('PyPI server response is invalid: missing "releases" key in response')
-            return 0 
-        downloads = 0
-        for version, variants in content['releases'].items():
-            for variant in variants:
-                downloads += variant['downloads']
-        LOGGER.debug('number of downloads for package {} is {}'.format(package_name, downloads))
-        return downloads 
+            data = { 'error': errno.EIO, 'impact': None }
+        else:
+            content = await response.json()
+            downloads = 0
+            for version, variants in content['releases'].items():
+                for variant in variants:
+                    downloads += variant['downloads']
+            data = { 'impact': downloads }
+            LOGGER.debug('Impact for module {} is {}'.format(json_request['module'], data['impact']))
+        return json.dumps(data)
 
 if __name__ == '__main__':
-    AsyncTCPCallbackServer(
-            callback = get_downloads, 
-            host = '0.0.0.0',
-            port = 25000,
-            encode = lambda d: bytes(str(d) + '\n', 'utf-8'),
-            decode = lambda d: base64.b64decode(d).decode().strip()
-            ).run()
+    AsyncTcpCallbackServer('0.0.0.0', 25000, get_downloads, memoized = True).run()
